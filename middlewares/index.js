ed3a1930f8c84650286e808685936a98661f8fb7
Parterdev <litemeilateran@outlook.com>
2022-11-02 12:02:40 -0500

    Apply centralize file to create  middlewares functions

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,27 +1,54 @@
 // Using express validator middleware to validate body fields
-const { check } = require('express-validator');
-const { validateReqFields } = require('./validateReqFields');
+const { request, response } = require('express');
+const { check, validationResult } = require('express-validator');
 const { validationMessages } = require('../helpers/strings');
 const { 
   isAValidRole, 
-  isEmailDuplicate,
+  isEmailDuplicate, 
   searchUserById 
-} = require('../helpers/index');
-// Control middlewares for http actions (API)
-const { 
-  postMiddleware,
-  putMiddleware
-} = require('./users/index');
+} = require('../helpers/index'); 
+
+// To return an array of middlewares (using in routes)
+const middleware = {
+  postMiddleware: () => {
+    return [
+      // Using middlewares to intercept controller execution
+      check('name', validationMessages.nameIsRequired).not().isEmpty(),
+      check('email', validationMessages.emailError).isEmail(),
+      check('password', validationMessages.passwordLenghtError).isLength({ min: 8 }),
+  
+      /* Validatig roles with hardcoded array params
+      check('role', validationMessages.notValidRole).isIn(['ADMIN_ROLE', 'USER_ROLE']), */
+  
+      // Validating with match DB data
+      check('role').custom(isAValidRole),
+      check('email').custom(isEmailDuplicate)
+    ]
+  },
+  putMiddleware: () => {  
+    return [
+      check('id', validationMessages.notValidID).isMongoId(),
+      check('email', validationMessages.emailError).isEmail(),
+      check('id').custom(searchUserById),
+      check('role').custom(isAValidRole).optional()
+    ]
+  },
+};
 
 
+// To validate middleware chain errors
+const validateReqFields = (req=request, res=response, next) => {
+  // Catching express validator middleware errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+  next();
+};
 
 module.exports = {
-  check,
-  isAValidRole,
-  isEmailDuplicate,
-  postMiddleware,
-  putMiddleware,
-  searchUserById,
-  validationMessages,
-  validateReqFields,
+  middleware,
+  validateReqFields
 }
